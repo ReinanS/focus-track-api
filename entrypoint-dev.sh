a32408e3713edb8515502912e452ec387d332d45
ReinanS <reinandesouza01@gmail.com>
2025-07-20 22:30:33 -0300

    chore: add ci/cd

---
+++ entrypoint-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Executa as migrações do banco de dados
+poetry run alembic upgrade head
+
+# Inicia a aplicação com debugpy
+poetry run python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn focus_track_api.app:app --host 0.0.0.0 --port 8000
